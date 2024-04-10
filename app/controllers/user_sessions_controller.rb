2966d2c29378f3c091196bd567a9808a7f4faac4
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-04-11 01:37:05 +0900

    🚀 治らねえ

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,7 +2,7 @@ class UserSessionsController < ApplicationController
   def new; end
 
   def create
-    @user = login(params[:name], params[:email], params[:password]) 
+    @user = login(params[:email], params[:password]) 
     if @user.save
       redirect_to root_path, notice: 'Login successful'
     else
