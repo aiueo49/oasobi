b0111dc394eae305910286f33b26ad399eb8b745
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-04-11 01:17:24 +0900

    🚀 ログインできない問題を解消したい

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,7 +3,7 @@ class UserSessionsController < ApplicationController
 
   def create
     @user = login(params[:name], params[:email], params[:password]) 
-    if @user.present? && @user.save
+    if @user.save
       redirect_to root_path, notice: 'Login successful'
     else
       flash.now[:alert] = 'Login failed'
