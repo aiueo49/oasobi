8f9dbe99b396819039ec3bc315a3c7213cc43eb6
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-04-11 16:22:03 +0900

    🚀 パラムスネームはダメっぽい

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,7 +3,7 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-    @user = login(params[:name], params[:email], params[:password]) 
+    @user = login(params[:email], params[:password]) 
     if @user
       redirect_to root_path, notice: 'Login successful'
     else
